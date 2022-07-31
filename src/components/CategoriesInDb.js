ccc991c79dea253cf02974ff5ae4a7762bbb2362
JonatanAdrianIrigoytia <yonyirigoytia@gmail.com>
2022-07-31 13:21:58 -0300

    CategoriesInDb terminado

--- src/components/CategoriesInDb.js
+++ src/components/CategoriesInDb.js
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
-import {useState} from 'react';
-
-const estado = useState ([
-	fetch("http://localhost:3001/api/products")
-	.then(response => response.json())
-	.then(data =>this.state({ categories: data.countByCategory}))
-]);
-const categoriesList = estado[0];
-const setCategoriesList =  estado[1];
+import React, { useState, useEffect } from "react";
 
 function CategoriesInDb() {
-	const [category];
+	const [categoriesList, setCategoriesList] = useState([]);
 
-	useState([categoriesList, setCategoriesList])
+	useEffect(() => {
+		fetch("http://localhost:3001/api/products")
+			.then((response) => response.json())
+			.then((data) => setCategoriesList(data.countByCategory));
+	}, []);
 	return (
 		<div className="col-lg-6 mb-4">
 			<div className="card shadow mb-4">
 				<div className="card-header py-3">
-					<h5 className="m-0 font-weight-bold text-gray-800">
-						Categories
-					</h5>
+					<h5 className="m-0 font-weight-bold text-gray-800">Categories</h5>
 				</div>
 				<div className="card-body">
 					<div className="row">
-						{this.state.categories.map((category, index) => {
+						{categoriesList.map((category, index) => {
 							return (
 								<div className="col-lg-6 mb-4" key={category.category + index}>
 									<div className="card bg-dark text-white shadow">
