f6cfea130e9c5fd5f7041ae5db62d2f8f0372cc0
dpertierra <dpertierra@hotmail.com>
2022-07-31 15:04:06 -0300

    Creando componente para la tarjeta de categorias

--- src/components/CategoriesInDb.js
+++ src/components/CategoriesInDb.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import CategoryCard from "./CategoryCard";
 
 function CategoriesInDb() {
 	const [categoriesList, setCategoriesList] = useState([]);
@@ -17,16 +18,7 @@ function CategoriesInDb() {
 				<div className="card-body">
 					<div className="row">
 						{categoriesList.map((category, index) => {
-							return (
-								<div className="col-lg-6 mb-4" key={category.category + index}>
-									<div className="card bg-dark text-white shadow">
-										<div className="card-body">
-											<span>{category.category}</span>
-											<span>{category.count}</span>
-										</div>
-									</div>
-								</div>
-							);
+							return <CategoryCard {...category} key={index} />;
 						})}
 					</div>
 				</div>
