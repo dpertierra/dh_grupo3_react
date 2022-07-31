a6e2380b6838319dff9639263483803833f8a3b8
dpertierra <dpertierra@hotmail.com>
2022-07-31 15:10:08 -0300

    Agregando estilos

--- src/components/CategoriesInDb.js
+++ src/components/CategoriesInDb.js
@@ -13,7 +13,7 @@ function CategoriesInDb() {
 		<div className="col-lg-6 mb-4">
 			<div className="card shadow mb-4">
 				<div className="card-header py-3">
-					<h5 className="m-0 font-weight-bold text-gray-800">Categories</h5>
+					<h5 className="m-0 font-weight-bold .card-text-dark">Categories</h5>
 				</div>
 				<div className="card-body">
 					<div className="row">
